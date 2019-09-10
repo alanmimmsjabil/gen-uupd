dd5965d6229c2e763bcd3e57a355abec015f4d49
Alan Mimms <alanmimms@gmail.com>
2019-09-10 11:05:14 -0700

    Make python2 compatible

--- gen-unit-password.py
+++ gen-unit-password.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-import re
+from __future__ import print_function
 import sys
+
+if (sys.version_info > (3, 0)):
+    import secrets
+    RND = secrets
+else:
+    import random
+    RND = random
+
+import re
 import string
-import secrets
 
 if len(sys.argv) != 2:
     n = 16
@@ -10,7 +18,7 @@ else:
     n = int(sys.argv[1])
 
 alphabet = 'abcdefghijmnpqrstuvwxyz' + string.digits
-password = ''.join(secrets.choice(alphabet) for i in range(n))
+password = ''.join(RND.choice(alphabet) for i in range(n))
 
 password = re.sub(r'(....)', r'\1-', password)
 password = re.sub(r'-$', '', password)
